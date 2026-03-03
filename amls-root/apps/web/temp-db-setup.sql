0d2c43c3a9c3d2ad90a1e6deeb626746b4ee186d
IvanTham <Ivantham789@gmail.com>
2026-03-03 22:22:09 +0800

    no use files

--- amls-root/apps/web/temp-db-setup.sql
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://agxtlwhhmwonykclhppx.supabase.co'
-const supabaseKey = 'sb_publishable_986ZeRYGC4N827zrVKztww_eyi-NSHi'
-// We need the service role key to execute raw SQL, but since we don't have it,
-// we will instead use the REST API to execute postgres functions. 
-// Actually, let's use the standard `run_command` to execute the SQL securely via a postgres script or a migration endpoint if one exists.
-// A simpler way: we'll just write a script that attempts to insert into the tables. If it fails, we know they don't exist.
-// Since we don't have direct SQL access through the standard anon API, I'll instruct the user to run the SQL in their Supabase console.
--- amls-root/do_commit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d d:\Project\CariSkill\amls-root
-git add -A
-git commit -m "fix: unify AuthProvider, add api/chat to ai-worker, fix gemini model"
-git push origin csmerge
