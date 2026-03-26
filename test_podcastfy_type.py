1e7cb09d2dc3d82b1869014defbe5fba9402d21e
IvanTham <Ivantham789@gmail.com>
2026-03-26 23:58:16 +0800

    merge with cs branch

--- test_podcastfy_type.py
+++
@@ -1,7 +0,0 @@
-import inspect
-from podcastfy.client import generate_podcast
-
-if inspect.iscoroutinefunction(generate_podcast):
-    print("generate_podcast is ASYNC")
-else:
-    print("generate_podcast is SYNC")
